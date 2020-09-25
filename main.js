c6c84a9e109c56050587fbbec258e5e5b162644b
soupanova <duykhangkhuat@gmail.com>
2020-09-25 16:15:52 +0100

    a little more done

--- main.js
+++ main.js
@@ -1,48 +1,57 @@
-//Rock, paper, scissors.
-//
-let playerMove = "rock";
-let computerMove = "paper";
+//Rock, paper, scissors game
+//Variables declared for Player choice and Computer choice, message Prompt for player choice
+let playerMove = prompt ("SHOOT")
+let moves = ["paper", "rock", "scissors"]
+let computerMove = getRandomMove()
+
+
+//set of instructions for computer to randomly generate a move.
+function getRandomMove(){
+    return moves[Math.floor(Math.random() * 3)]    
+}
 
 function getWinner(playerMove, computerMove){
     //if playerMove is rock and computerMove is scissors then playerMove wins = 1 point
-    if (playerMove === "rock" && computerMove === "scissors") {
-        return "player wins"
+    if (playerMove.toLowerCase() === "rock" && computerMove === "scissors") {
+        return "player wins [1]"
     }
     //if playerMove is rock and computerMove is paper then playerMove loses = -1 point
-    else if (playerMove === "rock" && computerMove === "paper") {
-        return "player loses"
+    else if (playerMove.toLowerCase() === "rock" && computerMove === "paper") {
+        return "player loses [-1]"
     }
     //if playerMove is rock and computerMove is rock then playerMove draws = 0 point
-    else if (playerMove === "rock" && computerMove === "rock") {
-        return "player draws"
+    else if (playerMove.toLowerCase() === "rock" && computerMove === "rock") {
+        return "player draws [0]"
     }
     //if playerMove is scissors and computerMove is scissors then playerMove draws = 0 point
-    else if (playerMove === "scissors" && computerMove ==="scissors") {
-        return "player draws"
+    else if (playerMove.toLowerCase() === "scissors" && computerMove ==="scissors") {
+        return "player draws [0]"
     }
     //if playerMove is scissors and computerMove is paper then playerMove wins = 1 point
-    else if (playerMove === "scissors" && computerMove ==="paper") {
-        return "player draws"
+    else if (playerMove.toLowerCase() === "scissors" && computerMove ==="paper") {
+        return "player wins [1]"
     }
     //if playerMove is scissors and computerMove is rock then playerMove loses = -1 point
-    else if (playerMove === "scissors" && computerMove ==="rock") {
-        return "player draws"
+    else if (playerMove.toLowerCase() === "scissors" && computerMove ==="rock") {
+        return "player loses [0]"
     }
     //if playerMove is paper and computerMove is scissors then playerMove loses = -1 point
-    else if (playerMove === "paper" && computerMove ==="scissors") {
-        return "player draws"
+    else if (playerMove.toLowerCase() === "paper" && computerMove ==="scissors") {
+        return "player loses [0]"
     }
     //if playerMove is paper and computerMove is paper then playerMove draws = 0 point
-    else if (playerMove === "paper" && computerMove ==="paper") {
-        return "player draws"
+    else if (playerMove.toLowerCase() === "paper" && computerMove ==="paper") {
+        return "player draws [0]"
     }
     //if playerMove is paper and computerMove is rock then playerMove wins = 1 point
-    else (playerMove === "paper" && computerMove ==="rock"); {
-        return "player draws"
+    else (playerMove.toLowerCase() === "paper" && computerMove ==="rock"); {
+        return "player wins [1]"
     }
 }
 
-let result = getWinner("rock", "paper")
+//Testing the result of the match
+let result = getWinner(playerMove, computerMove)
+alert(result)
+let playAgain =  confirm("Would you like to play again?")
 
-function
-
+//We need our code to run on a loop 
